8556a5f98f4241b072c8c133445ce68ad8efc5b9
BrunoCiprianoDev <cipriano990@gmail.com>
2023-08-25 11:38:55 -0300

    feat: add main components

---
+++ docs/js/sidebar/Sidebar.js
@@ -0,0 +1,40 @@
+const sidebarOptions = [
+    {href: "/pages/page6.html", label: "Semana do Ensino"},
+    {href: "/pages/page5.html", label: "Semana do Técnico"},
+    {href: "*", label: "Semana da Cultura"},
+    {href: "*", label: "Seminario de extensão"},
+    {href: "*", label: "Simposio de Pesquisa Inovação e Pós-graduação"},
+    {href: "*", label: "Roda de Conversa Treinamento Profissional"},
+]
+
+class Sidebar extends HTMLElement {
+    constructor() {
+        super();
+        this.render();
+    }
+
+    render() {
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="/styles/sidebar.css">
+            <aside id="sidebar-container">
+                <ul>
+                    ${this.renderSidebarOptions()}
+                </ul>
+            </aside>`;
+    }
+
+    renderSidebarOptions() {
+        return sidebarOptions.map(
+            (option) => `
+                <li><a href="#">${option.label}</a></li>
+            `
+        ).join("");
+    }
+
+    connectedCallback(){
+
+    }
+}
+
+customElements.define("sidebar-component", Sidebar);
